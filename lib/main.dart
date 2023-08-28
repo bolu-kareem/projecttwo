f604d09fe691aca98b1a6c3f0619795da35ff477
bolu-kareem <76807014+bolu-kareem@users.noreply.github.com>
2023-08-28 13:29:31 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(const MaterialApp(
   home: QuoteList(),
@@ -18,36 +19,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(text: 'Find God', author: 'saka'),
   ];
 
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-            const SizedBox(height: 10.0,),
-          ]
-        ),
-      ) ,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,10 +31,12 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote)=> quoteTemplate(quote)).toList(),
+        children: quotes.map((quote)=> QuoteCard(quote: quote)).toList(),
       ),
     );
   }
 }
+
+
 
 
